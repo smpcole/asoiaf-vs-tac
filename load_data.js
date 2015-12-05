0ee59429623c8dd273a6ed3934a8372d6c2dda4e
Sam Cole <scole3@uic.edu>
2015-12-05 15:12:43 -0600

    Add newNode() function and index.html for testing

---
+++ load_data.js
@@ -0,0 +1,23 @@
+// Stores all created nodes, indexed by their unique 2-letter keys
+var nodes = {};
+
+/* Return a new node object & add it to list of nodes
+ *
+ * name: character's full name
+ * key: character's 2-letter key
+ * neighbors: list of connected characters' 2-letter keys
+ * pos: object containing fields x & y representing center of node's circle
+ */
+function newNode(name, key, neighbors, pos) {
+	var node = {
+		name: name,
+		key: key,
+		neighbors: neighbors,
+		pos: pos
+	};
+	nodes[key] = node;
+	
+	// TODO: draw the node
+
+	return node;
+}
