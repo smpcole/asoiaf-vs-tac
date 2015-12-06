01c06148976c56aacdd8ae836998bd689edb7022
Sam Cole <scole3@uic.edu>
2015-12-06 09:36:54 -0600

    Move blurbs to blurbs.js

---
+++ blurbs.js
@@ -0,0 +1,13 @@
+blurbs = {};
+
+for(i = 0; i < tac_chars.length; i++)
+	blurbs[tac_chars[i]] = {};
+
+// Clemence-Sansa
+blurbs["ch"]["ss"] = "Blindly enamored of betrothed";
+
+// Marguerite-Cersei
+blurbs["mb"]["cl"] = "Imprisoned for cheating on husband";
+
+// Marguerite-Sansa
+blurbs["mb"]["ss"] = "Discarded for a more politically advantageous bride";
