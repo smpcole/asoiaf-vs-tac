466cab2a0e691f86be4ffbee810f03dcb0a145d6
Sam Cole <scole3@uic.edu>
2015-12-07 22:42:26 -0600

    Show blurbs when mousing over edges

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var LABEL_WIDTH = 200,
 	VTX_RAD = 4,
 	VTX_DIST = 40, // Distance between centers of two consecutive vertices
 	LABEL_OFFSET = 10, // Distance between label and center of vertex
-	MARGIN = {top: VTX_RAD, bottom: VTX_RAD, left: 0, right: 0};
+	BLURB_WIDTH = 150,
+	BLURB_HEIGHT = 100,
+	MARGIN = {top: BLURB_HEIGHT / 2, bottom: BLURB_HEIGHT / 2, left: 0, right: 0};
 
 // Overall width of the graphics
 var WIDTH = GRAPH_WIDTH + 2 * (LABEL_WIDTH + LABEL_OFFSET);
@@ -71,6 +73,9 @@ function vertexClicked(char_ID, i) {
 	svg.selectAll("line")
 		.attr("visibility", "hidden");
 
+	// Also hide any visible blurb
+	hideBlurb();
+
 	// Show edges incident to the clicked vertex
 	svg.selectAll("[data-l=" + char_ID + "], [data-r=" + char_ID + "]")
 		.attr("visibility", "visible");
@@ -95,4 +100,31 @@ svg.selectAll("dummy")
 	.attr("data-l", function(e) {return e.l;}) // Store char IDs of left and right endpoints so that we can easily select all edges with a given endpoint
 	.attr("data-r", function(e) {return e.r;})
 	.attr("visibility", "hidden")
-	.style({stroke: "black", "stroke-width": "2px"});
+	.style({stroke: "black", "stroke-width": "2px"})
+	.on("mouseover", showBlurb);
+
+function hideBlurb() {
+	console.log("hideBlurb");
+	svg.selectAll(".blurb")
+		.remove();
+}
+
+function showBlurb(e, i) {
+	hideBlurb();
+	console.log("showBlurb");
+
+	// Top left of rectangle
+	var x = (LEFT_X + RIGHT_X - BLURB_WIDTH) / 2,
+		y = (char_dict[e.l].y + char_dict[e.r].y - BLURB_HEIGHT) / 2;
+
+	svg.append("foreignObject")
+		.classed("blurb", true)
+		.attr("x", x)
+		.attr("y", y)
+		.attr("width", BLURB_WIDTH)
+		.attr("height", BLURB_HEIGHT)
+	  .append("xhtml:body")
+		.html(blurbs[e.l][e.r]);
+
+}
+
