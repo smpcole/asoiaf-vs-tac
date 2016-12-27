smpcole/asoiaf-vs-tac

commit 1b16103cbbbaf31c73d78c842c2c01594a82ec72

Add about authors link
Issue #8

main.js
@@ -232,3 +232,31 @@ req.onload = function() {
 	}
 };
 req.send();
+
+function aboutAuthors() {
+	var grrm = {
+		name: "George R. R. Martin",
+		id: "grrm",
+		series: "asoiaf"
+	};
+
+	var md = {
+		name: "Maurice Druon",
+		id: "md",
+		series: "tac"
+	};
+
+	var e = {
+		tac: md.name,
+		asoiaf: grrm.name
+	};
+
+	// Clear the side panel
+	sidePanel.select("#instructions").remove();
+	sidePanel.selectAll("div").html("");
+
+	showInfo(grrm, sidePanel.select("#selected-char"));
+	showInfo(md, sidePanel.select("#connected-char"));
+	showConnections(e);
+	
+};

blurbs/md-grrm
@@ -1,0 +1,1 @@
+Both wrote/are writing awesome series of novels!

index.html
@@ -53,6 +53,9 @@
 </div>
 
 <br>
+<!-- 'return false;' prevents it from following the link -->
+<a href="#" onclick="aboutAuthors();return false;">About the authors</a><br>
+<br>
 <a href="https://github.com/smpcole/asoiaf-vs-tac">Contribute on GitHub</a>
 
 <script src="chars.js"></script>

commit 9aeb78b15fb13c5aa437062b4f56dbd4e4e160a4

Fix error in aboutAuthors()
Edge was constructed using the authors' names rather than IDs, so the blurb
wasn't found.

Broken in 1b16103cbbbaf31c73d78c842c2c01594a82ec72

main.js
@@ -247,8 +247,8 @@ function aboutAuthors() {
 	};
 
 	var e = {
-		tac: md.name,
-		asoiaf: grrm.name
+		tac: md.id,
+		asoiaf: grrm.id
 	};
 
 	// Clear the side panel