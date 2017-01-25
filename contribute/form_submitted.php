138e982f0f6d76419858439f22ff5949d02beb59
Sam Cole <scole3@uic.edu>
2017-01-24 21:31:09 -0600

    Add function to compose message for email

--- contribute/form_submitted.php
+++ contribute/form_submitted.php
@@ -22,6 +22,29 @@ if(isset($_POST["submit"])) {
     $name = get_input("name");
     $citeas = get_input("citeas");
     $link = get_input("link");
+
+    $msg = compose_msg($asoiaf, $tac, $connections, $name, $email, $citeas, $link);
+}
+
+function compose_msg($asoiaf, $tac, $connections, $name, $email, $citeas, $link) {
+	$msg = "<html><body>";
+
+	$msg .= "<p>Dear " . $name . ",</p><p>Thank you for contributing to The Original GOT!  I'll get back you as soon as I can.</p><p>See ya,</p><p>Sam</p><hr>";
+
+	$msg .= "<b><i>A Song of Ice and Fire</i> character:</b> " . $asoiaf . "<br><br>";
+
+	$msg .= "<b><i>The Accursed Kings</i> character:</b> " . $tac . "<br><br>";
+
+	$connections = str_replace("\n", "</p><p>", $connections);
+	$msg .= "<b>Connections:</b><p>" . $connections . "</p>";
+
+	$msg .= "<b>Submitted by:</b><br>" . $name;
+	$msg .= "<br><a href=\"mailto:" . $email . "\">" . $email . "</a><br><br>";
+
+	$msg .= "<b>Cite as:</b><br>" . $citeas . "<br><a href=\"" . $link . "\">" . $link . "</a>";
+
+	$msg .= "</body></html>";
+	return $msg;
 }
 
 function error($msg) {
