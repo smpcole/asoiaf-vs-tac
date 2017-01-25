8e8a590569a94b45ede8d3c520dc9d1111658627
Sam Cole <scole3@uic.edu>
2017-01-24 19:21:47 -0600

    Check for empty connections

--- contribute/form_submitted.php
+++ contribute/form_submitted.php
@@ -2,7 +2,17 @@
 
 if(isset($_POST["submit"])) {
 	// TODO: protect against multiple submission
-	echo "Form submitted";
+
+	// Check for empty connections
+	$connections = get_input("connections");
+	if(empty($connections)) {
+		error("Please write something in the \"connections\" box.");
+		return;
+	}
+}
+
+function error($msg) {
+	echo "<font color=\"red\">" . $msg . "</font>";
 }
 
 function get_input($name) {
