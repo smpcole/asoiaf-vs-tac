7af7fbfe5d6400667895ed8552144c31a1ef1ddc
Sam Cole <scole3@uic.edu>
2017-12-13 23:26:16 -0600

    Don't allow arbitrary domains in wikis.php
    This may protect against malicious use.  I'm not sure if it was possible
    in the first palce, but better be safe than sorry...

--- wikis.php
+++ wikis.php
@@ -1,6 +1,8 @@
 <?php
 $handle = rawurlencode($_GET["handle"]);
 $domain = $_GET["domain"];
+if($domain != "en.wikipedia.org/w" && $domain != "awoiaf.westeros.org")
+    return;
 $url = "http://" . $domain . "/api.php?action=query&redirects&format=json&titles=" . $handle . "&prop=extracts&utf8&exintro&exsentences=3&explaintext";
 echo file_get_contents($url);
 ?>
